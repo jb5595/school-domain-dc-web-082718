22da2a9f9140f48b4a81f07ef5fc9c5b05c45ed3
Jacob Berman <jacobberman1995@gmail.com>
2018-08-29 16:58:03 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,5 +8,7 @@ class School
   def add_student(student_name, student_grade)
    if  !@roster.has_key?(student_grade)
      roster[student_grade] = []
+   end 
+   roster[student_grade] << student_name
   end 
 end 
